ba6a6aba73410e3f981d6011e35755483f37a566
unknown <eddy211817@gmail.com>
2024-11-21 18:43:31 -0500

    docs improved

--- client.py
+++ client.py
@@ -3,139 +3,155 @@ import string
 import socket
 import argparse
 import multiprocessing as mp
-from concurrent.futures import ProcessPoolExecutor  
+from concurrent.futures import ProcessPoolExecutor
 from threading import Thread
 import logging
 
 VALID_CHARS = string.ascii_letters + string.digits
-# character to signal end of comunication in socket
-END = "*".encode()
+END_SIGNAL = "*".encode()  # Character to signal end of communication in socket
 
 
-def receive_results(s: socket.socket,buffer_size: int)->None:
-    with open('results.txt', 'wb',buffering=buffer_size) as f:
+def receive_results(s: socket.socket, buffer_size: int) -> None:
+    """Receive results from the server and write them to a file."""
+    with open('results.txt', 'wb', buffering=buffer_size) as f:
         while data := s.recv(buffer_size):
-            if data.endswith(END):
-                f.write(data[:-len(END)])
+            if data.endswith(END_SIGNAL):
+                f.write(data[:-len(END_SIGNAL)])
                 break
             f.write(data)
 
+
 def build_chain() -> str:
-    chain_length = random.randint(50,101)
+    """Build a random chain of characters with spaces."""
+    chain_length = random.randint(50, 101)
     num_spaces = random.randint(3, 5)
-    
+
     valid_spaces = []
-    while len(valid_spaces)<num_spaces:
+    while len(valid_spaces) < num_spaces:
         choice = random.randint(1, chain_length - 1)
-        if (choice not in valid_spaces 
-        and choice-1 not in valid_spaces 
-        and choice+1 not in valid_spaces):
+        if (choice not in valid_spaces
+            and choice-1 not in valid_spaces
+                and choice+1 not in valid_spaces):
             valid_spaces.append(choice)
 
     chain_chars = list(random.choices(VALID_CHARS, k=chain_length))
     for pos in valid_spaces:
         chain_chars[pos] = ' '
-    return ''.join(chain_chars)+"\n"
+    return ''.join(chain_chars) + "\n"
 
-def generate_chunck(size: int):
+
+def generate_chunk(size: int):
+    """Generate a chunk of random character chains."""
     return [build_chain() for _ in range(size)]
 
-def generate_chains_no_parallel(num_chains: int,s: socket.socket,buffer_size: int,chunck_size: int) -> None:
-    with open('chains.txt', 'w',buffering=buffer_size) as f:
-        def send_lines(lines : list[str]):
-            f.writelines(lines)
-            s.sendall("".join(lines).encode())
-        while num_chains > 0:
-            if num_chains <= chunck_size:
-                send_lines(generate_chunck(num_chains))
-                break
-            num_chains -= chunck_size
-            send_lines(generate_chunck(chunck_size))     
-        s.sendall(END)
 
-def generate_chains(num_chains: int,s: socket.socket,buffer_size: int,chunck_size: int,parallel: int)-> None:
-    if parallel == 1:
-        return generate_chains_no_parallel(num_chains,s,buffer_size,chunck_size)
-    with open('chains.txt', 'w',buffering=buffer_size) as f,ProcessPoolExecutor(parallel) as pool:
-        def send_lines(lines : list[str]):
+def generate_chains_no_parallel(num_chains: int, s: socket.socket, buffer_size: int, chunk_size: int) -> None:
+    """Generate chains without parallelism."""
+    with open('chains.txt', 'w', buffering=buffer_size) as f:
+        def send_lines(lines: list[str]):
             f.writelines(lines)
             s.sendall("".join(lines).encode())
+
         while num_chains > 0:
-            if num_chains <= chunck_size:
-                send_lines(generate_chunck(num_chains))
+            if num_chains <= chunk_size:
+                send_lines(generate_chunk(num_chains))
                 break
-            q,r = divmod(num_chains,chunck_size)
+            num_chains -= chunk_size
+            send_lines(generate_chunk(chunk_size))
+        s.sendall(END_SIGNAL)
+
+
+def generate_chains(num_chains: int, s: socket.socket, buffer_size: int, chunk_size: int, parallel: int) -> None:
+    """Generate chains with optional parallelism."""
+    if parallel == 1:
+        return generate_chains_no_parallel(num_chains, s, buffer_size, chunk_size)
+
+    with open('chains.txt', 'w', buffering=buffer_size) as f, ProcessPoolExecutor(parallel) as pool:
+        def send_lines(lines: list[str]):
+            f.writelines(lines)
+            s.sendall("".join(lines).encode())
+
+        while num_chains > 0:
+            if num_chains <= chunk_size:
+                send_lines(generate_chunk(num_chains))
+                break
+
+            q, r = divmod(num_chains, chunk_size)
             if q >= parallel:
-                chunks = [ chunck_size ] * parallel
-                num_chains -= parallel * chunck_size 
+                chunks = [chunk_size] * parallel
+                num_chains -= parallel * chunk_size
             else:
                 num_chains = 0
-                chunks = [ chunck_size ] * q
+                chunks = [chunk_size] * q
                 if r != 0:
                     chunks.append(r)
-            for lines in pool.map(generate_chunck,chunks):
-                send_lines(lines)      
-        s.sendall(END)
 
-if __name__ == "__main__":
+            for lines in pool.map(generate_chunk, chunks):
+                send_lines(lines)
+
+        s.sendall(END_SIGNAL)
+
+
+def main():
     args_parser = argparse.ArgumentParser()
 
-    args_parser.add_argument("-c",
-                            default=1_000_000,
-                            help=f"number of chains to generate , default 1_000_000",
-                            type=int)
+    args_parser.add_argument("-c", "--chains", default=1_000_000,
+                             help=f"number of chains to generate , default 1_000_000",
+                             type=int)
 
-    args_parser.add_argument("-a",
-                            default="localhost:3000",
-                            help=f"socket address , default localhost:3000")
+    args_parser.add_argument("-a", "--address", default="localhost:3000",
+                             help=f"socket address , default localhost:3000")
 
-    args_parser.add_argument("-p",
-                            default=1,
-                            help=f"parallelism , default no parallelism(1)",
-                            type=int)
+    args_parser.add_argument("-p", "--parallel", default=1,
+                             help=f"parallelism , default no parallelism",
+                             type=int)
 
-    
-    args_parser.add_argument("-b",
-                            default=1024 * 1024,
-                            help=f"buffer size for socket and files in bytes , default 1 MB",
-                            type=int)
+    args_parser.add_argument("-b", "--buffer", default=1024 * 1024,
+                             help=f"buffer size for socket and files in bytes , default 1 MB",
+                             type=int)
 
-    
-    args_parser.add_argument("-n",
-                            default=10_000,
-                            help=f"number of chains to assign to each parrallel process , default 10_000",
-                            type=int)
+    args_parser.add_argument("-n", "--chunck", default=10_000,
+                             help=f"number of chains to assign to each parallel process , default 10_000",
+                             type=int)
 
     args = args_parser.parse_args()
-
-    logging.basicConfig(level=logging.INFO,format='%(asctime)s %(levelname)s:%(message)s')
 
     num_chains: int = args.c
     assert num_chains > 0
 
-    buffer_size : int = args.b
+    buffer_size: int = args.b
     assert buffer_size > 0
 
-    parallel : int = args.p
-    assert parallel > 0 
+    parallel: int = args.p
+    assert parallel > 0
+
     cpus = mp.cpu_count()
     if parallel > cpus:
-        logging.info(f"paralleism {cpus} because pc only haves {cpus} cores")
-        parallel = mp.cpu_count()
+        logging.info(f"Parallelism limited to {cpus}")
+        parallel = cpus
 
-    chunck_size : int = args.n
-    assert chunck_size > 0
+    chunk_size: int = args.n
+    assert chunk_size > 0
 
     try:
         host, port = args.a.split(":")
         port = int(port)
         address = (host, port)
     except ValueError as e:
-        raise ValueError("Invalid socket address format. Expected format: host:port") from e
+        raise ValueError(
+            "Invalid socket address format. Expected format: host:port") from e
 
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client_socket:
         client_socket.connect(address)
-        receiver = Thread(target=receive_results,args=[client_socket,buffer_size])
+        receiver = Thread(target=receive_results, args=[
+                          client_socket, buffer_size])
         receiver.start()
-        generate_chains(num_chains,client_socket,buffer_size,chunck_size,parallel)
+
+        generate_chains(num_chains, client_socket,
+                        buffer_size, chunk_size, parallel)
+
         receiver.join()
+
+
+if __name__ == "__main__":
+    main()
--- server.py
+++ server.py
@@ -7,76 +7,92 @@ import socket
 import time
 import multiprocessing as mp
 
-INVALID_CHAIN_WEIGHT = 1000
-# character to signal end of comunication in socket
-END = "*"
 
 def process_connection(queue: mp.Queue, conn_socket: socket.socket, remote_address: str, buffer_size: int):
+    """Handles incoming client connections."""
+
+    INVALID_CHAIN_WEIGHT = 1000
+    END_SIGNAL = "*"  # Character to signal end of communication in socket
+
     logger = setup_logger(queue)
     logger.info(f"Connection from {remote_address}")
 
     def get_weight(chain: str) -> float:
+        """Calculates the weight of the given chain based on specific rules."""
+
         if 'aa' in chain.lower():
             logger.warning(f"Double 'a' rule detected >> '{chain.strip()}'")
             return INVALID_CHAIN_WEIGHT
+
         digits = sum(1 for x in chain if x.isdigit())
         spaces = chain.count(' ')
         letters = len(chain) - digits - spaces
+
         if spaces == 0:
             logger.warning(f"Chain '{chain}' has no spaces")
             return INVALID_CHAIN_WEIGHT
+
         return (letters * 1.5 + digits * 2) / spaces
 
     with conn_socket:
-        start_time = time.perf_counter()
+        start_time = time.perf_counter_ns()
         end_received = False
         pending = None
+
         try:
             while not end_received and (data := conn_socket.recv(buffer_size)):
                 chains = data.decode().split("\n")
                 last = chains[-1]
-                if last.endswith(END):
-                    last = last.removesuffix(END)
+
+                # Check if end signal is present
+                if last.endswith(END_SIGNAL):
+                    last = last.removesuffix(END_SIGNAL)
                     if last:
                         chains[-1] = last
                     else:
                         chains.pop()
                     end_received = True
+
                 if pending:
                     chains[0] = pending + chains[0]
-                pending = last    
-                results = [f"{chain} : {weight:.2f}" for chain in chains 
-                            if (weight := get_weight(chain)) != INVALID_CHAIN_WEIGHT]
-                conn_socket.sendall("\n".join(results).encode())    
+                pending = last
+
+                results = [f"{chain} : {weight:.2f}" for chain in chains
+                           if (weight := get_weight(chain)) != INVALID_CHAIN_WEIGHT]
+
+                conn_socket.sendall("\n".join(results).encode())
+
         except BrokenPipeError as e:
             logger.error(f"Error while handling {remote_address}: {e}")
         except KeyboardInterrupt:
             return
-        conn_socket.sendall((pending + END).encode())
-        elapsed_time = time.perf_counter() - start_time
-        logger.info(f"Process from {remote_address} completed in {elapsed_time:.2f} seconds.")
+
+        # Send any pending data before closing connection
+        conn_socket.sendall((pending + END_SIGNAL).encode())
+
+        elapsed_time_ms = (time.perf_counter_ns() - start_time) // 1_000_000
+        logger.info(f"Process from {remote_address} completed in {
+                    elapsed_time_ms} ms")
+
 
 def setup_logger(queue: mp.Queue):
-    logger = logging.getLogger() 
-    logger.setLevel(logging.INFO)
+    """Sets up the logger to use multiprocessing queue."""
+
+    logger = logging.getLogger()
     handler = logging.handlers.QueueHandler(queue)
     logger.addHandler(handler)
-    
     return logger
 
-def handle_logs(queue: mp.Queue, log_filename: str):
-    # Check if log file exists for rotation
-    if os.path.isfile(log_filename):
-        old_file = f'{log_filename}_{datetime.datetime.now().strftime("%d_%m_%Y_%H_%M_%S")}'
-        os.rename(log_filename, old_file)
 
-    # Create file handler for logging
-    root_logger = logging.getLogger()  
+def handle_logs(queue: mp.Queue, log_filename: str):
+    """Handles logging from the queue to a file."""
+
+    root_logger = logging.getLogger()
     handler = logging.FileHandler(log_filename)
-    formatter = logging.Formatter('%(asctime)s - %(processName)s - %(levelname)s - %(message)s')
+    formatter = logging.Formatter(
+        '%(asctime)s - %(processName)s - %(levelname)s - %(message)s')
     handler.setFormatter(formatter)
     root_logger.addHandler(handler)
-
 
     while (record := queue.get()) is not None:
         try:
@@ -85,24 +101,22 @@ def handle_logs(queue: mp.Queue, log_filename: str):
             root_logger.error(f"Error handling log record: {e}")
 
 
-if __name__ == "__main__":
+def main():
     args_parser = argparse.ArgumentParser()
 
-    args_parser.add_argument("-l", default="server.log",
+    args_parser.add_argument("-l", "--logfile", default="server.log",
                              help=f"log file name, default server.log")
 
-    args_parser.add_argument("-a",default="localhost:3000",
-                            help=f"socket address , default localhost:3000")
+    args_parser.add_argument("-a", "--address", default="localhost:3000",
+                             help=f"socket address , default localhost:3000")
 
-
-    args_parser.add_argument("-b",
-                            default= 1024 * 1024,
-                            help=f"buffer size for socket and files in bytes , default 1 MB",
-                            type=int)
+    args_parser.add_argument("-b", "--buffer", default=1024 * 1024,
+                             help=f"buffer size for socket and files in bytes , default 1 MB",
+                             type=int)
 
     args = args_parser.parse_args()
 
-    buffer_size : int = args.b
+    buffer_size: int = args.b
     assert buffer_size > 0
 
     log_filename = args.l
@@ -112,27 +126,37 @@ if __name__ == "__main__":
         port = int(port)
         address = (host, port)
     except ValueError as e:
-        raise ValueError("Invalid socket address format. Expected format: host:port") from e
+        raise ValueError(
+            "Invalid socket address format. Expected format: host:port") from e
 
     queue = mp.Queue(-1)
-  
+
     log_listener = mp.Process(target=handle_logs, args=(queue, log_filename))
     log_listener.start()
-    
+
     # Setup the main server logger without console output.
     logger = setup_logger(queue)
 
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server_socket:
         server_socket.bind(address)
-        server_socket.listen()   
+        server_socket.listen()
+        logger.info(f"server started at {address}")
         try:
             while True:
                 conn_socket, remote_address = server_socket.accept()
-                mp.Process(target=process_connection,args=[queue, conn_socket, remote_address, buffer_size]).start()     
+                mp.Process(target=process_connection, args=[
+                           queue, conn_socket, remote_address, buffer_size]).start()
+
         except KeyboardInterrupt:
             logger.info("Shutting down server...")
+
         except Exception as e:
-            logger.info(f"Server is shutting down because of {e}")  
+            logger.error(f"Server is shutting down because of {e}")
+
         finally:
-            queue.put_nowait(None)   #Signal the log listener to exit
-            log_listener.join()      #Wait for the log listener to finish
+            queue.put_nowait(None)   # Signal the log listener to exit
+            log_listener.join()      # Wait for the log listener to finish
+
+
+if __name__ == "__main__":
+    main()
